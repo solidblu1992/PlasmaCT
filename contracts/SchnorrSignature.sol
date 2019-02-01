1340bd4c4fb4d38b95f64d654703c4dd0aa38a75
solidblu1992 <alegreve1992@gmail.com>
2019-01-31 19:28:29 -0600

    Add files via upload

--- contracts/SchnorrSignature.sol
+++ contracts/SchnorrSignature.sol
@@ -121,53 +121,18 @@ library SchnorrSignature {
 		if (sig.length == 0) return false;
 		if (sig.length != P.length) return false;
 		
-		uint i;
-		for (i = 0; i < P.length; i++) {
-			if (!P[i].IsOnCurve()) return false;
-		}
-		
 		//For a valid signature: sum{s}*G = sum{R} - sum{ekPk}
 		//or sum{ekPk} = sum{R} - sum{s}*G
-		//Note: Don't use RecoverMultiple as additional optimzations can be done
-		//e.g.  can sum s-values as scalars instead of G1Points
-		//      this also includes the single mod-inverse optimization
-		uint[] memory e_inv = new uint[](sig.length);
+		G1Point.Data memory P_sum_right = RecoverMultipleSum(sig);
 		
-		G1Point.Data memory right = sig[0].R;
-		uint s_sum = sig[0].s;
-		
-		for (i = 0; i < sig.length; i++) {
-			//Check that R is on the curve
-			assert(sig[i].R.IsOnCurve());
-			
-			//Get e-value
-			e_inv[i] = uint(keccak256(abi.encodePacked(sig[i].R.x, sig[i].R.y, sig[i].message)));
-			
-			if (i > 0) {
-				//i == 0: the initial value is already stored in s_sum and right before the for loop
-				
-				//Sum s-values
-				s_sum = Scalar.Add(s_sum, sig[i].s);
-				
-				//Sum R-points
-				right = right.Add(sig[i].R);
-			}
+		//Calculate sum{Pk}
+		G1Point.Data memory P_sum_left = P[0];
+		for (uint i = 1; i < sig.length; i++) {
+		    if (!P[i].IsOnCurve()) return false;
+			P_sum_left = P_sum_left.Add(P[i]);
 		}
 		
-		//Subtract of sG
-		right = right.Add(G1Point.MultiplyG1(s_sum).Negate());
-		
-		//Calculate batch inverse
-		e_inv = Vector.Inverse(e_inv);
-		
-		//Calculate sum{ekPk}
-		G1Point.Data memory left;
-				
-		for (i = 0; i < sig.length; i++) {
-			left = left.Add(G1Point.Multiply(P[i], e_inv[i]));
-		}
-		
-		return left.Equals(right);
+		return P_sum_left.Equals(P_sum_right);
 	}
 	
 	function GetHash(Data memory sig) internal pure returns (bytes32 hash) {
