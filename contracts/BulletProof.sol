4231e90ffe0bb82e535ec4ddacc2432429697653
solidblu1992 <alegreve1992@gmail.com>
2019-02-25 20:23:33 -0600

    Update BulletProof.sol

--- contracts/BulletProof.sol
+++ contracts/BulletProof.sol
@@ -325,7 +325,7 @@ library BulletProof {
 	    require(gi_scalars.length > 0);
 	    require(gi_scalars.length == hi_scalars.length);
 	    require(start < gi_scalars.length);
-	    require(count < gi_scalars.length);
+	    require(count <= gi_scalars.length);
 	    require((start+count) < gi_scalars.length);
 	    
 	    //Set initial point if given (to allow multiexp to be broken up into multiple steps)
